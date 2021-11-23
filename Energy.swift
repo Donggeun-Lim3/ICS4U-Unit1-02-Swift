305d1c3093d25bac1fb63d63d962b36ec2a8ff6a
Ubuntu <ubuntu@ip-172-31-82-219.ec2.internal>
2021-11-23 03:54:50 +0000

    try

---
+++ Energy.swift
@@ -0,0 +1,38 @@
+/*
+This program gets mass of object from the user's input,
+calculates and display energy of object in Joules and
+tells input is not a number, if inout is not a number
+author  Donggeun Lim
+version 1.0
+since   2021-11-21
+*/
+
+func BoardFoot(mass : Double) -> Double {
+ // Calculate energy
+ let energy = mass * 2.998e8 * 2.998e8
+
+ // Return values
+ return energy
+}
+
+
+enum InvalidInputError : Error {
+  case invalidInput
+}
+// Input
+print("Enter the mass of an object in kilograms: ", terminator: "")
+let massString = readLine()
+
+do {
+ guard let mass = Double(massString!)
+ else{
+   throw InvalidInputError.invalidInput
+ }
+    let energy = BoardFoot(mass: mass)
+
+    // Output
+    print("\(mass) kg of mass would produce \(energy)J of energy.")
+} catch {
+  print("Sorry, that was not an number")
+}
+print("\nDone.")
